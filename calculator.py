8df7c73f5ea6a9ba050fcb33ee7d1c7ba1421a2a
gra-ch <chengracecwy@gmail.com>
2021-07-24 19:57:16 +0800

    Add testing

--- calculator.py
+++ calculator.py
@@ -1,17 +1,23 @@
-# calculator function that can add, subtract, multiply and divide using functions
-class calculator():
-    def add(x, y):
-        return x + y
+def add(x, y):
+    return x + y
 
-    def subtract(x, y):
-        return x - y
 
-    def multiply(x, y):
-        return x * y
+def subtract(x, y):
+    return x - y
 
-    def divide(x, y):
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    try:
         return x / y
+    except:
+        raise ValueError("You can't divide by zero")
 
+
+def main():
     print("Select operation.")
     print("1.Add")
     print("2.Subtract")
@@ -23,18 +29,20 @@ class calculator():
     num2 = int(input("Enter second number: "))
 
     if choice == '1':
-        print(num1,"+",num2,"=", add(num1,num2))
+        print(num1, "+", num2, "=", add(num1, num2))
 
     elif choice == '2':
-        print(num1,"-",num2,"=", subtract(num1,num2))
+        print(num1, "-", num2, "=", subtract(num1, num2))
 
     elif choice == '3':
-        print(num1,"*",num2,"=", multiply(num1,num2))
+        print(num1, "*", num2, "=", multiply(num1, num2))
 
     elif choice == '4':
-        print(num1,"/",num2,"=", divide(num1,num2))
+        print(num1, "/", num2, "=", divide(num1, num2))
 
     else:
         print("Invalid input")
 
-calculator()
+
+if __name__ == '__main__':
+    main()
